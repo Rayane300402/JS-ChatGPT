258680d69f32d6d2f334788fc88eb1b4a359f8c1
Rayane Naboulsi <rayanenaboulsi02@gmail.com>
2023-04-22 21:54:24 +0300

    url change

--- client/script.js
+++ client/script.js
@@ -93,7 +93,7 @@ const handleSubmit = async (e) => {
   loader(messageDiv)
 
   // fetch the response from the server
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://codexalapanda.onrender.com', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json',
